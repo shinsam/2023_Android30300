ff7ec1b6b80eb265ef0c4988a3c5e4627943f10a
user <shin321gg@gmail.com>
2023-04-25 14:46:06 +0900

    first

---
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "안녕", Toast.LENGTH_SHORT).show();
+    }
+}
